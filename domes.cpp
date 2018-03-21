ef2bf4dffe04384febcd7f112f635ccfe7e9d250
Spyro <spiros_giannikakis@windowslive.com>
2018-03-21 17:23:38 +0200

    First commit

---
+++ domes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+
+int main(void) {
+
+  int aks_size; //μεγεθος πινακα των αξιοθεατων
+  int aks[aks_size]; // πινακας των αξιοθεατων
+  int vath[aks_size]; //πινακας βαθμολογιων των αξιοθεατων
+  int k; //Αριθμος ημερών
+
+  // cout << "Δωσε Αριθμο ημερών: ";
+  // cin >> k;
+  // cout << "Δωσε πληθος αξιοθεατων: ";
+  // cin >> aks_size;
+  //
+  // for(int i = 0; i < aks_size; i++){
+  //   std::cout << "Αξιολογησε το " << i << "αξιοθεατο: ";
+  //   cin >> vath[i];
+  // }
+
+
+
+
+
+
+  return 0;
+}
