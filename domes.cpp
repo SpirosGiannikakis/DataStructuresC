9543351aaf226236fd3069ecc0a316c8da7a8fd7
Spyro <spiros_giannikakis@windowslive.com>
2018-03-21 21:22:28 +0200

    Allages prwths meras (proodos 0) :( { Anamonh gia eisodo programmatos }

--- domes.cpp
+++ domes.cpp
@@ -1,28 +1,101 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
-#include<list>
+#include <list>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
 
 using namespace std;
 
 
-int main(void) {
+int main () {
 
-  int aks_size; //μεγεθος πινακα των αξιοθεατων
-  int aks[aks_size]; // πινακας των αξιοθεατων
-  int vath[aks_size]; //πινακας βαθμολογιων των αξιοθεατων
-  int k; //Αριθμος ημερών
 
-  // cout << "Δωσε Αριθμο ημερών: ";
-  // cin >> k;
-  // cout << "Δωσε πληθος αξιοθεατων: ";
-  // cin >> aks_size;
-  //
-  // for(int i = 0; i < aks_size; i++){
-  //   std::cout << "Αξιολογησε το " << i << "αξιοθεατο: ";
-  //   cin >> vath[i];
-  // }fbvxvvzvsz
-afruawfafuawfuawfawufawbfawufafnuawfnaufnawf
-asdadasdasd
-  return 0;
+  char s[40] = "1256 8942 1965 42";
+
+  int n = atoi(s);
+
+  cout << n << endl;
+
+  int j = 0;
+  for(int i = 0; i < strlen(s); i++){
+    if(s[i] == ' ') {
+
+    }
+
+  }
+
+
 }
+
+//   list<double> eisodos;
+//
+//
+//
+//   string line;
+//   ifstream myfile in("test.txt");
+//   if (myfile.is_open())
+//
+//     while (!myfile.eof())
+//     {
+//       double d;
+//       in >> d;
+//     }
+//     myfile.close();
+//     return 0;
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// template<class T>
+// struct node {
+//   T info;
+//   node *next;
+// };
+//
+// template<class T>
+// class list {
+// private:
+//   //pointer typou node gia to telos kai tin arxi
+//   node *head, *tail;
+// public:
+//   //constructor
+//   list(){
+//     head=NULL;
+//     tail=NULL;
+//   }
+// };
+//
+// template<class T>
+// void  push_back(T value){
+//   node *temp=new node;
+//   temp->info=value;
+//   temp->next=NULL;
+//
+//   if(head==NULL) {
+//     head=temp;
+//     tail=temp;
+//     temp=NULL;
+//   }
+//
+//   else {
+//     tail->next=temp;
+//     tail=temp;
+//   }
+// }
